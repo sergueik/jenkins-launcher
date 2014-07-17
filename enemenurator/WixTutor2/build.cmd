db591fcb183f262b81d39ac86cfe2a300b5e6965
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-16 21:50:26 -0400

    merged UI capable wix project with the legacy one

---
+++ enemenurator/WixTutor2/build.cmd
@@ -0,0 +1,3 @@
+msbuild.exe /property:WixSdkPath=c:\wix\sdk\ /property:WixTasksPath=c:\wix\WixTasks.dll /property:WixCATargetsPath=c:\wix\SDK\wix.ca.targets /property:WixTargetsPath=c:\wix\wix.targets /property:WixTasksPath=c:\wix\WixTasks.dll /property:WixToolPath=c:\wix\  WixDataBase.sln  /t:clean,rebuild
+
+
