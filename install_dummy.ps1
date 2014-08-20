0b879c4034aec40e9a7acc5f1de0398c00d4fdb2
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-08-20 13:47:34 -0400

    sample node configuration

--- install_dummy.ps1
+++ install_dummy.ps1
@@ -1,12 +1,30 @@
-$jenkins_slave_home='D:\java\Jenkins\slave\bin'
-pushd $jenkins_slave_home 
-set-location -Path 'basicService'
-invoke-expression -command "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe WindowsService.sln"
+$drive = 'c:'
+$selenium_node_home ="${drive}\selenium-local"
+$dummy_service_home = "${drive}\selenium-local\BasicService"
+$dummy_binary = 'WindowsService.exe'
+$binary_path = "${dummy_service_home}\WindowsService\bin\Debug\${dummy_binary}"
+
+$ServiceName = 'selenium-node'
+pushd $selenium_node_home  -erroraction  SilentlyContinue
+set-location -Path $dummy_service_home
+<# 
+
+WindowsService.sln(2): Solution file error MSB5014: File format version is not r
+ecognized.  MSBuild can only read solution files between versions 7.0 and 9.0, i
+nclusive.
+#>
+
+
+$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$msbuild = "C:\Windows\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe"
+invoke-expression -command "${msbuild} WindowsService.sln"
+
+
 set-location -path 'WindowsService\bin\Debug'
-copy-item -Path "*.*" -Destination $jenkins_slave_home -force -Include '*' -recurse
+copy-item -Path "*.*" -Destination $selenium_node_homr -force -Include '*' -recurse
 popd
 
-$ServiceName = 'dummy_service_3'
+
 $service = Get-WmiObject -Class Win32_Service -Filter "Name='${ServiceName}'"
 
 if ($service -ne $null){
@@ -23,7 +41,7 @@ if ($service -ne $null){
 
 
 write-output "Installing service ""${ServiceName}"""
-invoke-expression -command 'c:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -install "D:\java\Jenkins\slave\bin\WindowsService.exe"'
+invoke-expression -command "c:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -install ""${binary_path}"""
 
 <# 
 if ($false) {
@@ -47,5 +65,5 @@ if ($false) {
 }
 	# Without credentials service is set to run under LocalSystem
 	# on Windows Server 2012 the account should be LocalServiced 
-        New-Service -name "dummy_service_3" -Description 'This service runs Jenkins Slave Agent' -binaryPathName "D:\java\jenkins\slave\bin\WindowsService.exe" -displayName "dummy_service_3" -startupType Automatic
+        New-Service -name "dummy_service_3" -Description 'This service runs selenium-node Slave Agent' -binaryPathName "D:\java\selenium-node\master\WindowsService.exe" -displayName "dummy_service_3" -startupType Automatic
 #>
