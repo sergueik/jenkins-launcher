2e85710c984d5e00d184c81cc5b380e434dddec5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-08-20 13:48:14 -0400

    sample node configuration

--- update_dummy_service.ps1
+++ update_dummy_service.ps1
@@ -1,32 +1,58 @@
-$ServiceName = 'dummy_service_3'
+# this script tweaks  the dummy service  installed on Windows  platform and  swaps the name of the executable
+# to compensate for the issues winsw has with Windows 2012 .
+
+
+$ServiceName = 'selenium-node'
+$drive = 'C:'
+
+$selenium_node_home ="${drive}\selenium-local"
+$dummy_service_home = "${drive}\selenium-local\BasicService"
+
+$dummy_binary = 'WindowsService.exe'
+
+$binary_path = "${dummy_service_home}\WindowsService\bin\Debug\${dummy_binary}"
+
+$real_binary  = "selenium-node.exe"
+$realbinary_path = "${selenium_node_home}\${real_binary}"
+
 $service = Get-WmiObject -Class Win32_Service -Filter "Name='${ServiceName}'"
 
+
 if ($service -ne $null){
-$binaryPath = "d:\java\Jenkins\slave\bin\WindowsService.exe"
-$realbinaryPath = "d:\java\Jenkins\slave\bin\jenkins.exe"
+
+
+
 write-output "Stopped service ${ServiceName}"
 invoke-expression -command 'c:\windows\system32\sc.exe stop "${ServiceName}"'
 
-@('jenkins.err.log', 'jenkins.out.log', 'jenkins.wrapper.log') | foreach-object {set-content -Path $_ -value ''}
+write-output "Truncating selenium-node logs"
+<#
+Prevent error:
+Service cannot be started. System.UnauthorizedAccessException: Access to the path 'd:\java\selenium-node\slave\bin\selenium-node.wrapper.log' is denied.
+#>
 
+@('selenium-node.err.log', 'selenium-node.out.log', 'selenium-node.wrapper.log') | foreach-object {set-content -Path $_ -value ''}
+# TODO: ACL
+
+write-output "Changing service parameters in the registry"
 pushd HKLM:
 cd /System/CurrentControlSet/Services
 cd "${ServiceName}"
 get-itemproperty -name 'ImagePath' -path "HKLM:\System\CurrentControlSet\Services\${ServiceName}"
-set-itemproperty -name 'ImagePath' -path "HKLM:\System\CurrentControlSet\Services\${ServiceName}" -value $realbinaryPath
+set-itemproperty -name 'ImagePath' -path "HKLM:\System\CurrentControlSet\Services\${ServiceName}" -value $realbinary_path
+write-output "Changed service ${ServiceName} binary Path to ""${realbinary_path}"""
 popd
-write-output "Change service ${ServiceName} binary Path to ""${realbinaryPath}"""
-<#
-Possible error:
-Service cannot be started. System.UnauthorizedAccessException: Access to the path 'd:\java\Jenkins\slave\bin\jenkins.wrapper.log' is denied.
 
+write-output 'Change service identity to LocalSystem'
+invoke-expression -command 'c:\windows\system32\sc.exe config ${ServiceName} obj= LocalSystem'
 
-#>
+pushd $selenium_node_home
 
-invoke-expression -command 'c:\windows\system32\sc.exe config "dummy_service_3" obj= LocalSystem'
+invoke-expression -command "c:\windows\system32\sc.exe start ""${ServiceName}"""
 write-output "Started service ${ServiceName}"
-invoke-expression -command 'c:\windows\system32\sc.exe start "${ServiceName}"'
 start-sleep 15
-get-content -path 'jenkins.wrapper.log' | out-string
+write-output "Waiting for java logs ${ServiceName}"
+get-content -path 'selenium-node.wrapper.log' | out-string
+popd 
 
 }
