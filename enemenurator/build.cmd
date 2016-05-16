274b9ca15136759052910d291f483774ac00078d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-16 11:18:43 -0400

    remove unrelated project

--- enemenurator/build.cmd
+++
@@ -1,5 +0,0 @@
-REM 
-
-msbuild.exe enemenurator.csproj 
-msbuild.exe /property:WixSdkPath=c:\wix\sdk\ /property:WixTasksPath=c:\wix\WixTasks.dll /property:WixCATargetsPath=c:\wix\SDK\wix.ca.targets /property:WixTargetsPath=c:\wix\wix.targets /property:WixTasksPath=c:\wix\WixTasks.dll /property:WixToolPath=c:\wix\ enemenurator.wproj  /t:clean,rebuild
-
